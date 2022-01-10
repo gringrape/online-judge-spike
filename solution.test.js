37f5bb15eb970d140bbccad85f71d97f4fc6568d
gringrape <gringrape200@gmail.com>
2022-01-11 04:45:41 +0900

    단순한 함수문자열을 실행하는 함수 구현

---
+++ solution.test.js
@@ -0,0 +1,62 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+const fs = require('fs').promises;
+
+async function execute(code, testCase) {
+  const { input, output: expectedOutput } = testCase;
+
+  const inputString = input.join(',');
+
+  const executableCode = `
+  const func = ${code};
+  
+  console.log(func(${inputString}));
+  `;
+
+  const path = './test';
+  await fs.writeFile(path, executableCode, 'utf8');
+
+  const { stdout } = await exec(`node ${path}`);
+
+  const result = JSON.stringify(JSON.parse(stdout));
+  const expected = JSON.stringify(expectedOutput);
+
+  return expected === result;
+}
+
+test('excutes simple function', async () => {
+  const code = 'function add() { return 3; }';
+  const testCase = {
+    input: [],
+    output: 3,
+  };
+
+  const result = await execute(code, testCase);
+
+  expect(result).toBe(true);
+});
+
+test('excutes sum function', async () => {
+  const code = 'function add(a, b) { return a + b; }';
+  const testCase = {
+    input: [4, 9],
+    output: 13,
+  };
+
+  const result = await execute(code, testCase);
+
+  expect(result).toBe(true);
+});
+
+test('excutes complex function', async () => {
+  const code = 'function add(a, b, c) { return [a, b, c]; }';
+  const testCase = {
+    input: [3, 9, 5],
+    output: [3, 9, 5],
+  };
+
+  const result = await execute(code, testCase);
+
+  expect(result).toBe(true);
+});
